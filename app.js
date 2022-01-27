9b11508e0b9f1b94818d44509c3cd7a2b0271923
Parkhi <parkhigarg277@gmail.com>
2022-01-27 23:22:46 +0530

    fixing errors

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const port = process.env.PORT || 3000;
+require("./src/db/conn");
+const UserRouter = require("./routers/User")
+
+const dotenv = require('dotenv');
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+app.use("/api/users",UserRouter);
+
+app.listen(port,()=>
+{
+    console.log(`connection succesful  at port ${port}`);
+})
