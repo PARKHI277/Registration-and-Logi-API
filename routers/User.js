4936b8ae2a9b89b8a1de75d2a7c73206980d1cbb
Parkhi <parkhigarg277@gmail.com>
2022-01-31 15:04:09 +0530

    adding otp

--- routers/User.js
+++ routers/User.js
@@ -29,13 +29,11 @@ router.post("/register",async(req,res)=>
     //check if email exixt already
     const  emailExist = await User.findOne({email: req.body.email});
     if(emailExist) return res.status(400).send('Email already exixts');
-    
-    
     const userpassword = req.body.password;
     const otp = Math.floor(Math.floor(100000 + Math.random() * 900000));
     const strongPasswords = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/;
     if(strongPasswords.test(userpassword))
-    {
+{
     //Hash Passwords
     const salt =  await bcrypt.genSalt(10);
     const hashPassword = await bcrypt.hash(userpassword,salt);
@@ -52,12 +50,12 @@ router.post("/register",async(req,res)=>
         isverified:false,
         plainpassword:userpassword,
         otpuser:otp   
-      } 
-       );
+      });
     /*const token = jwt.sign({_id:user.id},'secret');
     const token = createtoken(user._id);
     //adding cookie
     res.cookie('jwtg',token,{httpOnly:true,maxAge:maxAge*1000});*/
+    console.log(user.otpuser);
     console.log(otp);
     user.save().then(()=>
     {  
@@ -70,14 +68,56 @@ router.post("/register",async(req,res)=>
    }).catch((err)=>{
     res.status(400).send(err);
 })
+       
 }
 else
 {
-    res.send("Please enter strong password");
+    return res.send("Please enter strong password");
 }
-
-
 });
+
+router.post('/otp-send',async(req,res,next) =>
+{    
+    const userexixt = await User.findOne({email: req.body.email});
+    
+    if(userexixt){
+    try{    
+        console.log(userexixt.otpuser)
+           const transporter = nodemailer.createTransport({
+                 service:"gmail",
+                 auth:{
+                     user : "codechef277@gmail.com",
+                     pass:"Apiuser@1234"
+                 }
+             });
+             const mailOptions = {
+                 from:"codechef277@gmail.com",
+                 to:userexixt.email,
+                 subject:"Your otp for verification",
+                 text: userexixt.otpuser
+             };
+             transporter.sendMail(mailOptions,function(error,info){
+                 if(error)
+                 {
+                     console.log(error);
+                 }
+                 else
+                 {
+                     console.log("Otp sent to your entered email");
+                 }
+            })
+             res.status(201).send("otp has been sent to your email")
+            }catch(err)
+            {
+                res.status(400).send("Something went wrong");
+            }
+    }
+    else
+    {
+        res.send("Please enter valid email id")
+    }
+ })
+
 
 router.post("/login",async(req,res) => {
     const user = await User.findOne({email: req.body.email});
@@ -120,48 +160,6 @@ router.get("/login",async(req,res)=>
 })
 
 
-//otp sending to email
-router.post('/otp-send',async(req,res,next) =>
-{    
-    const userexixt = await User.findOne({email: req.body.email});
-    
-    if(userexixt){
-    try{    
-        console.log(userexixt.otpuser)
-           const transporter = nodemailer.createTransport({
-                 service:"gmail",
-                 auth:{
-                     user : "codechef277@gmail.com",
-                     pass:"Apiuser@1234"
-                 }
-             });
-             const mailOptions = {
-                 from:"codechef277@gmail.com",
-                 to:userexixt.email,
-                 subject:"Your otp for verification",
-                 text: userexixt.otpuser
-             };
-             transporter.sendMail(mailOptions,function(error,info){
-                 if(error)
-                 {
-                     console.log(error);
-                 }
-                 else
-                 {
-                     console.log("Otp sent to your entered email");
-                 }
-            })
-             res.status(201).send("otp has been sent to your email")
-            }catch(err)
-            {
-                res.status(400).send("Something went wrong");
-            }
-    }
-    else
-    {
-        res.send("Please enter valid email id")
-    }
- })
 
   // Forgot password router
 router.get('/forgot-password',(req,res,next) =>
@@ -262,5 +260,7 @@ router.get('/verify', (req, res) => {
   })*/
 
 
+
+  
 
 module.exports = router;
