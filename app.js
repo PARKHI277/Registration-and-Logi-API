a9babfd44183a46f3532fcdf1eaa901ab83aa65c
Parkhi <parkhigarg277@gmail.com>
2022-02-01 14:42:32 +0530

    change

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const express = require('express');
-const cors = require('cors');
+// const cors = require('cors');
 require("./src/db/conn");
 const UserRouter = require("./routers/User")
 
@@ -8,7 +8,7 @@ const UserRouter = require("./routers/User")
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
 
 // app.use((req ,res ,next)=>{
 //     res.setHeader('Access-Control-Allow-Origin','*');
